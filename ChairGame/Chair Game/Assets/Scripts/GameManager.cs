66630d0b45e88bc66c7de38744401f3332a441a0
Kylar Black <kylar.c.black@gmail.com>
2023-02-05 03:03:36 -0600

    Scenes, and GM script

---
+++ ChairGame/Chair Game/Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager gm;
+
+    private void Awake()
+    {
+        if (gm == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            gm = this;
+        }
+        else if (gm != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public static void LoadScene(int _sceneIndex)
+    {
+        SceneManager.LoadScene(_sceneIndex);
+    }
+}
